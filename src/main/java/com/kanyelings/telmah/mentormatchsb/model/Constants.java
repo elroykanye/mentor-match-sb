2ef4033d4cb7a38a0d284b4d33a6bd5aabf18d40
elroykanye <elroykanye@gmail.com>
2021-10-16 16:42:17 +0100

    complete matching algorithm: modified ./src/main/java/com/kanyelings/telmah/mentormatchsb/model/Constants.java

--- src/main/java/com/kanyelings/telmah/mentormatchsb/model/Constants.java
+++ src/main/java/com/kanyelings/telmah/mentormatchsb/model/Constants.java
@@ -1,2 +1,15 @@
-package com.kanyelings.telmah.mentormatchsb.model;public class Constants {
+package com.kanyelings.telmah.mentormatchsb.model;
+
+import com.kanyelings.telmah.mentormatchsb.entity.MentorEntity;
+
+public class Constants {
+    public static final MentorEntity DEFAULT_MENTOR = MentorEntity.builder()
+            .firstName("Elroy")
+            .secondName("Kanye")
+            .department("COME")
+            .phoneNumber("672270627")
+            .gender("M")
+            .mentorId(-25L)
+            .build();
+
 }
