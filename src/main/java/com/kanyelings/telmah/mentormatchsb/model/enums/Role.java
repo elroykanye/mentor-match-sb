02372d1b8d52cef3ecdeca4f2bae8eb7a8abd0c1
elroykanye <elroykanye@gmail.com>
2021-10-26 07:37:12 +0100

    enum for repping entity role

---
+++ src/main/java/com/kanyelings/telmah/mentormatchsb/model/enums/Role.java
@@ -0,0 +1,12 @@
+package com.kanyelings.telmah.mentormatchsb.model.enums;
+
+public enum Role {
+    MENTEE("mentee"),
+    MENTOR("mentor");
+
+    String role;
+
+    Role(String role) {
+        this.role = role;
+    }
+}
