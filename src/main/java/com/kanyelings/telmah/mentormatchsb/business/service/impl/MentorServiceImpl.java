9b3ce5d7e1e609da07146b0a124d7fa68452e48d
elroykanye <elroykanye@gmail.com>
2021-10-26 18:33:50 +0100

    rearranged packages to abcde format: modified ./src/main/java/com/kanyelings/telmah/mentormatchsb/business/service/impl/MentorServiceImpl.java

--- src/main/java/com/kanyelings/telmah/mentormatchsb/business/service/impl/MentorServiceImpl.java
+++ src/main/java/com/kanyelings/telmah/mentormatchsb/business/service/impl/MentorServiceImpl.java
@@ -1,8 +1,8 @@
-package com.kanyelings.telmah.mentormatchsb.service.impl;
+package com.kanyelings.telmah.mentormatchsb.business.service.impl;
 
-import com.kanyelings.telmah.mentormatchsb.entity.MentorEntity;
-import com.kanyelings.telmah.mentormatchsb.repository.MentorRepository;
-import com.kanyelings.telmah.mentormatchsb.service.MentorService;
+import com.kanyelings.telmah.mentormatchsb.business.service.MentorService;
+import com.kanyelings.telmah.mentormatchsb.data.entity.MentorEntity;
+import com.kanyelings.telmah.mentormatchsb.data.repository.MentorRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
