7ea3351fadb2b5be020371683970c49751d376b1
elroykanye <elroykanye@gmail.com>
2021-10-26 18:33:50 +0100

    rearranged packages to abcde format: modified ./src/main/java/com/kanyelings/telmah/mentormatchsb/data/entity/MentorEntity.java

--- src/main/java/com/kanyelings/telmah/mentormatchsb/data/entity/MentorEntity.java
+++ src/main/java/com/kanyelings/telmah/mentormatchsb/data/entity/MentorEntity.java
@@ -1,4 +1,4 @@
-package com.kanyelings.telmah.mentormatchsb.entity;
+package com.kanyelings.telmah.mentormatchsb.data.entity;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
