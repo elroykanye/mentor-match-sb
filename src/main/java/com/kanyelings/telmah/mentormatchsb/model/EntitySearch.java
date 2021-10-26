6d258579c0554ee6fd8455bae40693a03f3c666a
elroykanye <elroykanye@gmail.com>
2021-10-26 06:09:02 +0100

    impl search api

---
+++ src/main/java/com/kanyelings/telmah/mentormatchsb/model/EntitySearch.java
@@ -0,0 +1,40 @@
+package com.kanyelings.telmah.mentormatchsb.model;
+
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.kanyelings.telmah.mentormatchsb.model.enums.Role;
+import lombok.*;
+
+import javax.persistence.Entity;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class EntitySearch {
+
+    @JsonProperty(value = "id")
+    private Long id;
+
+    @JsonProperty(value = "role")
+    private Role role;
+
+    @JsonProperty(value = "firstName")
+    private String firstName;
+
+    @JsonProperty(value = "secondName")
+    private String secondName;
+
+    @JsonProperty(value = "phoneNumber")
+    private String phoneNumber;
+
+    @JsonProperty(value = "department")
+    private String department;
+
+    @JsonProperty(value = "gender")
+    private String gender;
+
+    @JsonProperty (value = "previousSchool")
+    private String previousSchool;
+}
