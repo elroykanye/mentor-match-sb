5ff519222daa9009a8df4c1e42d7988e7630f925
elroykanye <elroykanye@gmail.com>
2021-10-26 20:29:16 +0100

    mappwea, dtos and responses: modified ./src/main/java/com/kanyelings/telmah/mentormatchsb/business/mapper/MenteeMapper.java

--- src/main/java/com/kanyelings/telmah/mentormatchsb/business/mapper/MenteeMapper.java
+++ src/main/java/com/kanyelings/telmah/mentormatchsb/business/mapper/MenteeMapper.java
@@ -1,2 +1,16 @@
-package com.kanyelings.telmah.mentormatchsb.business.mapper;public interface MenteeMapper {
+package com.kanyelings.telmah.mentormatchsb.business.mapper;
+
+import com.kanyelings.telmah.mentormatchsb.api.dto.MenteeDto;
+import com.kanyelings.telmah.mentormatchsb.data.entity.MenteeEntity;
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring", implementationPackage = "<PACKAGE_NAME>.impl")
+public interface MenteeMapper {
+    MenteeDto mapMenteeEntityToDto(MenteeEntity menteeEntity);
+
+    @InheritInverseConfiguration
+    @Mapping(target = "menteeId", ignore = true)
+    MenteeEntity mapDtoToMentorEntity(MenteeDto menteeDto);
 }
