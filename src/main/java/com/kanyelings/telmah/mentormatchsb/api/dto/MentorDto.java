47d572eab0a7241a4fe4118a03f7190afedbcf07
elroykanye <elroykanye@gmail.com>
2021-10-26 20:29:16 +0100

    mappwea, dtos and responses: modified ./src/main/java/com/kanyelings/telmah/mentormatchsb/api/dto/MentorDto.java

--- src/main/java/com/kanyelings/telmah/mentormatchsb/api/dto/MentorDto.java
+++ src/main/java/com/kanyelings/telmah/mentormatchsb/api/dto/MentorDto.java
@@ -1,2 +1,29 @@
-package com.kanyelings.telmah.mentormatchsb.api.dto;public class MentorDto {
+package com.kanyelings.telmah.mentormatchsb.api.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+
+@AllArgsConstructor @NoArgsConstructor
+@Getter @Setter @Builder @ToString
+public class MentorDto {
+    @JsonProperty("mentee_id")
+    private Long mentorId;
+
+    @JsonProperty(value = "first_name")
+    private String firstName;
+
+    @JsonProperty(value = "second_name")
+    private String secondName;
+
+    @JsonProperty(value = "phone_number")
+    private String phoneNumber;
+
+    @JsonProperty(value = "wa_number")
+    private String waNumber;
+
+    @JsonProperty(value = "department")
+    private String department;
+
+    @JsonProperty(value = "gender")
+    private String gender;
 }
