e1034523e4b9583bcca1d6bf3b2f8f72cb4202db
elroykanye <elroykanye@gmail.com>
2021-10-26 20:29:16 +0100

    mappwea, dtos and responses: modified ./src/main/java/com/kanyelings/telmah/mentormatchsb/business/service/impl/MenteeServiceImpl.java

--- src/main/java/com/kanyelings/telmah/mentormatchsb/business/service/impl/MenteeServiceImpl.java
+++ src/main/java/com/kanyelings/telmah/mentormatchsb/business/service/impl/MenteeServiceImpl.java
@@ -1,26 +1,31 @@
 package com.kanyelings.telmah.mentormatchsb.business.service.impl;
 
-import com.kanyelings.telmah.mentormatchsb.data.entity.MenteeEntity;
+import com.kanyelings.telmah.mentormatchsb.api.dto.MenteeDto;
+import com.kanyelings.telmah.mentormatchsb.business.mapper.MenteeMapper;
 import com.kanyelings.telmah.mentormatchsb.data.repository.MenteeRepository;
 import com.kanyelings.telmah.mentormatchsb.business.service.MenteeService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
 public class MenteeServiceImpl implements MenteeService {
-
+    private final MenteeMapper menteeMapper;
     private final MenteeRepository menteeRepository;
 
     @Override
-    public List<MenteeEntity> getAllMentees() {
-        return menteeRepository.findAll();
+    public List<MenteeDto> getAllMentees() {
+        return menteeRepository.findAll()
+                .stream()
+                .map(menteeMapper::mapMenteeEntityToDto)
+                .collect(Collectors.toList());
     }
 
     @Override
-    public void addNewMentee(MenteeEntity newMentee) {
-        menteeRepository.save(newMentee);
+    public void addNewMentee(MenteeDto newMentee) {
+        menteeRepository.save(menteeMapper.mapDtoToMentorEntity(newMentee));
     }
 }
