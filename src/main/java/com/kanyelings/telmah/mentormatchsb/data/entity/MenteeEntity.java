af796f426b01ac960e4bc0fbb46bb21f865c73f4
elroykanye <elroykanye@gmail.com>
2021-10-26 20:29:16 +0100

    mappwea, dtos and responses: modified ./src/main/java/com/kanyelings/telmah/mentormatchsb/data/entity/MenteeEntity.java

--- src/main/java/com/kanyelings/telmah/mentormatchsb/data/entity/MenteeEntity.java
+++ src/main/java/com/kanyelings/telmah/mentormatchsb/data/entity/MenteeEntity.java
@@ -1,6 +1,5 @@
 package com.kanyelings.telmah.mentormatchsb.data.entity;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
 import javax.persistence.*;
@@ -13,24 +12,17 @@ public class MenteeEntity {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long menteeId;
 
-    @JsonProperty(value = "firstName")
     private String firstName;
 
-    @JsonProperty(value = "secondName")
     private String secondName;
 
-    @JsonProperty(value = "phoneNumber")
     private String phoneNumber;
 
-    @JsonProperty(value = "waNumber")
     private String waNumber;
 
-    @JsonProperty(value = "department")
     private String department;
 
-    @JsonProperty(value = "gender")
     private String gender;
 
-    @JsonProperty (value = "previousSchool")
     private String previousSchool;
 }
