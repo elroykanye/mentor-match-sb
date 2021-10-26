a035938202ec867caebc083e1dcde3be2430a386
elroykanye <elroykanye@gmail.com>
2021-10-26 22:56:59 +0100

    enum for repping entity role

--- src/main/java/com/kanyelings/telmah/mentormatchsb/MentorMatchSbApplication.java
+++ src/main/java/com/kanyelings/telmah/mentormatchsb/MentorMatchSbApplication.java
@@ -2,6 +2,14 @@ package com.kanyelings.telmah.mentormatchsb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableSwagger2
@@ -12,4 +20,30 @@ public class MentorMatchSbApplication {
         SpringApplication.run(MentorMatchSbApplication.class, args);
     }
 
+    @Bean
+    public Docket mmApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(getApiInfo());
+    }
+
+    private ApiInfo getApiInfo() {
+        return new ApiInfoBuilder()
+                .title("Mentor Match API")
+                .version("1.0.0")
+                .description("API reactive docs for the mentor match services")
+                .contact(
+                        new Contact(
+                                "Elroy Kanye",
+                                "elroykanye.github.io/elroykanye",
+                                "elroykanye@gmail.com"
+                        )
+
+                )
+                .license("Apache License Version 2.0")
+                .build();
+    }
 }
