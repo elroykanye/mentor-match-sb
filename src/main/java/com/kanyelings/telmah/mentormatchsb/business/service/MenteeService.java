a7843d7032047574eb9dcc1e30201073049db3f3
elroykanye <elroykanye@gmail.com>
2021-10-26 20:29:16 +0100

    mappwea, dtos and responses: modified ./src/main/java/com/kanyelings/telmah/mentormatchsb/business/service/MenteeService.java

--- src/main/java/com/kanyelings/telmah/mentormatchsb/business/service/MenteeService.java
+++ src/main/java/com/kanyelings/telmah/mentormatchsb/business/service/MenteeService.java
@@ -1,11 +1,11 @@
 package com.kanyelings.telmah.mentormatchsb.business.service;
 
-import com.kanyelings.telmah.mentormatchsb.data.entity.MenteeEntity;
+import com.kanyelings.telmah.mentormatchsb.api.dto.MenteeDto;
 
 import java.util.List;
 
 public interface MenteeService {
-    List<MenteeEntity> getAllMentees();
+    List<MenteeDto> getAllMentees();
 
-    void addNewMentee(MenteeEntity newMentee);
+    void addNewMentee(MenteeDto newMentee);
 }
