82a3dc3b115e4533e00d5cb7610ac5a44a1ea585
elroykanye <elroykanye@gmail.com>
2021-10-26 20:29:16 +0100

    mappwea, dtos and responses: modified ./src/main/java/com/kanyelings/telmah/mentormatchsb/business/service/impl/MentorServiceImpl.java

--- src/main/java/com/kanyelings/telmah/mentormatchsb/business/service/impl/MentorServiceImpl.java
+++ src/main/java/com/kanyelings/telmah/mentormatchsb/business/service/impl/MentorServiceImpl.java
@@ -1,26 +1,31 @@
 package com.kanyelings.telmah.mentormatchsb.business.service.impl;
 
+import com.kanyelings.telmah.mentormatchsb.api.dto.MentorDto;
+import com.kanyelings.telmah.mentormatchsb.business.mapper.MentorMapper;
 import com.kanyelings.telmah.mentormatchsb.business.service.MentorService;
-import com.kanyelings.telmah.mentormatchsb.data.entity.MentorEntity;
 import com.kanyelings.telmah.mentormatchsb.data.repository.MentorRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
 public class MentorServiceImpl implements MentorService {
-
+    private final MentorMapper mentorMapper;
     private final MentorRepository mentorRepository;
 
     @Override
-    public List<MentorEntity> getAllMentors() {
-        return mentorRepository.findAll();
+    public List<MentorDto> getAllMentors() {
+        return mentorRepository.findAll()
+                .stream()
+                .map(mentorMapper::mapMentorEntityToDto)
+                .collect(Collectors.toList());
     }
 
     @Override
-    public void addNewMentor(MentorEntity newMentor) {
-        mentorRepository.save(newMentor);
+    public void addNewMentor(MentorDto newMentor) {
+        mentorRepository.save(mentorMapper.mapDtoToMentorEntity(newMentor));
     }
 }
