065189b47d4ab9e59e2fe617e6817c05db21ead5
elroykanye <elroykanye@gmail.com>
2021-11-01 07:08:56 +0100

    added image upload fn: modified ./src/main/java/com/kanyelings/telmah/mentormatchsb/business/util/FileUploadUtil.java

--- src/main/java/com/kanyelings/telmah/mentormatchsb/business/util/FileUploadUtil.java
+++ src/main/java/com/kanyelings/telmah/mentormatchsb/business/util/FileUploadUtil.java
@@ -1,2 +1,26 @@
-package com.kanyelings.telmah.mentormatchsb.business.util;public class FileUploadUtil {
+package com.kanyelings.telmah.mentormatchsb.business.util;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+public class FileUploadUtil {
+    public static void saveFile(String imageUploadDir, String fileName, MultipartFile file) throws IOException {
+        Path uploadPath = Paths.get(imageUploadDir);
+
+        if(!Files.exists(uploadPath)) Files.createDirectories(uploadPath);
+
+        try (InputStream inputStream = file.getInputStream()) {
+            Path imageFilePath = uploadPath.resolve(fileName);
+
+            Files.copy(inputStream, imageFilePath, StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e){
+            throw new IOException("Could not save image file: "+ fileName, e);
+        }
+    }
 }
