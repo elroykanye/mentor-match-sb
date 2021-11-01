e3fc8d6f503a96c6d93f27f8a99ceef32fe83d1d
elroykanye <elroykanye@gmail.com>
2021-11-01 07:08:58 +0100

    added image upload fn: modified ./src/main/java/com/kanyelings/telmah/mentormatchsb/business/mapper/MenteeMapper.java

--- src/main/java/com/kanyelings/telmah/mentormatchsb/business/mapper/MenteeMapper.java
+++ src/main/java/com/kanyelings/telmah/mentormatchsb/business/mapper/MenteeMapper.java
@@ -5,12 +5,17 @@ import com.kanyelings.telmah.mentormatchsb.data.entity.MenteeEntity;
 import org.mapstruct.InheritInverseConfiguration;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
 @Mapper(componentModel = "spring", implementationPackage = "<PACKAGE_NAME>.impl")
 public interface MenteeMapper {
+    @Mapping(target = "image", expression = "java(mapBytesArrayToImageFile(menteeEntity.getImage()))")
     MenteeDto mapMenteeEntityToDto(MenteeEntity menteeEntity);
 
     @InheritInverseConfiguration
     @Mapping(target = "menteeId", ignore = true)
+    @Mapping(target = "image", ignore = true)
     MenteeEntity mapDtoToMentorEntity(MenteeDto menteeDto);
+
 }
