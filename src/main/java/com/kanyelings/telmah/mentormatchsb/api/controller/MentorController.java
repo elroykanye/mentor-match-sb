68e115403d5b1492894dad3af52ba967ceee9afb
elroykanye <elroykanye@gmail.com>
2021-11-01 07:20:15 +0100

    added image support for mentor entity

--- src/main/java/com/kanyelings/telmah/mentormatchsb/api/controller/MentorController.java
+++ src/main/java/com/kanyelings/telmah/mentormatchsb/api/controller/MentorController.java
@@ -3,6 +3,7 @@ package com.kanyelings.telmah.mentormatchsb.api.controller;
 import com.kanyelings.telmah.mentormatchsb.api.dto.MentorDto;
 import com.kanyelings.telmah.mentormatchsb.business.service.MentorService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -23,7 +24,12 @@ public class MentorController {
         return mentorService.getAllMentors();
     }
 
-    @PostMapping(value = "/add")
+    @PostMapping(
+            value = "/add",
+            consumes = {
+                    MediaType.APPLICATION_JSON_VALUE,
+                    MediaType.MULTIPART_FORM_DATA_VALUE
+            })
     public ResponseEntity<String> addMentor(@ModelAttribute MentorDto newMentor){
         return mentorService.addNewMentor(newMentor);
     }
