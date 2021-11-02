b322b9f0a5b52a227f027bae2ca66799e1377352
elroykanye <elroykanye@gmail.com>
2021-11-03 00:23:12 +0100

    refined test api to return json response instead of console running

--- src/main/java/com/kanyelings/telmah/mentormatchsb/api/controller/TestController.java
+++ src/main/java/com/kanyelings/telmah/mentormatchsb/api/controller/TestController.java
@@ -1,10 +1,18 @@
 package com.kanyelings.telmah.mentormatchsb.api.controller;
 
+import com.kanyelings.telmah.mentormatchsb.api.dto.MenteeDto;
+import com.kanyelings.telmah.mentormatchsb.api.dto.MentorDto;
 import com.kanyelings.telmah.mentormatchsb.business.service.TestService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @AllArgsConstructor
@@ -13,7 +21,10 @@ public class TestController {
     private final TestService testService;
 
     @GetMapping(value = "/shuffle")
-    public void shuffleTest() {
-        testService.shuffleTest();
+    public ResponseEntity<Map<MentorDto, List<MenteeDto>>> shuffleTest(@RequestParam String secret) {
+        if (!secret.equals("kanyelroy")) {
+            return new ResponseEntity<>(Map.of(), HttpStatus.FORBIDDEN);
+        }
+        return testService.shuffleTest();
     }
 }
