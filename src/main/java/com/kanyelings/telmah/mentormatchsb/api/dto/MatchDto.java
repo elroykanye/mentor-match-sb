9827d9d31f5424db6d3c7e542b9075a26dc10093
elroykanye <elroykanye@gmail.com>
2021-11-03 01:13:58 +0100

    changed response body to a dto for match api : modified ./src/main/java/com/kanyelings/telmah/mentormatchsb/api/dto/MatchDto.java

--- src/main/java/com/kanyelings/telmah/mentormatchsb/api/dto/MatchDto.java
+++ src/main/java/com/kanyelings/telmah/mentormatchsb/api/dto/MatchDto.java
@@ -1,2 +1,19 @@
-package com.kanyelings.telmah.mentormatchsb.api.dto;public class MatchDto {
+package com.kanyelings.telmah.mentormatchsb.api.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class MatchDto {
+    @JsonProperty("mentor")
+    private MentorDto mentor;
+
+    @JsonProperty("mentees")
+    private List<MenteeDto> mentees;
 }
