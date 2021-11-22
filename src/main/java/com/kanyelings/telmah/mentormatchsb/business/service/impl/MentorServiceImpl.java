bc4bf2e0968d5a3cdb54779aff0a24429daf67df
elroykanye <elroykanye@gmail.com>
2021-11-22 14:34:50 +0100

    removed image upload functionality: modified ./src/main/java/com/kanyelings/telmah/mentormatchsb/business/service/impl/MentorServiceImpl.java

--- src/main/java/com/kanyelings/telmah/mentormatchsb/business/service/impl/MentorServiceImpl.java
+++ src/main/java/com/kanyelings/telmah/mentormatchsb/business/service/impl/MentorServiceImpl.java
@@ -3,8 +3,6 @@ package com.kanyelings.telmah.mentormatchsb.business.service.impl;
 import com.kanyelings.telmah.mentormatchsb.api.dto.MentorDto;
 import com.kanyelings.telmah.mentormatchsb.business.mapper.MentorMapper;
 import com.kanyelings.telmah.mentormatchsb.business.service.MentorService;
-import com.kanyelings.telmah.mentormatchsb.business.util.ImageUtil;
-import com.kanyelings.telmah.mentormatchsb.data.entity.MentorEntity;
 import com.kanyelings.telmah.mentormatchsb.data.repository.MentorRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -35,19 +33,7 @@ public class MentorServiceImpl implements MentorService {
     public ResponseEntity<String> addNewMentor(MentorDto newMentor) {
         mentorRepository.save(mentorMapper.mapDtoToMentorEntity(newMentor));
 
-        boolean saved = false;
-        try {
-            String imagePath = ImageUtil.saveUserImage(newMentor.getImage(), newMentor.getUsername());
-            MentorEntity mentorEntity = mentorMapper.mapDtoToMentorEntity(newMentor);
-            mentorEntity.setImagePath(imagePath);
-            mentorRepository.save(mentorEntity);
-            saved = true;
-        } catch (Exception ignored){
-        }
-
-        return saved ?
-                new ResponseEntity<>("Mentee added", HttpStatus.CREATED):
-                new ResponseEntity<>("Mentee not added", HttpStatus.CONFLICT);
+        return new ResponseEntity<>("Mentee added", HttpStatus.CREATED);
     }
 
     @Override
