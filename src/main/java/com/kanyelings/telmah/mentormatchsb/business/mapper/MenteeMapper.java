1c9e0967137cd3785248c66a6f697b588cece24b
elroykanye <elroykanye@gmail.com>
2021-11-22 14:34:50 +0100

    removed image upload functionality: modified ./src/main/java/com/kanyelings/telmah/mentormatchsb/business/mapper/MenteeMapper.java

--- src/main/java/com/kanyelings/telmah/mentormatchsb/business/mapper/MenteeMapper.java
+++ src/main/java/com/kanyelings/telmah/mentormatchsb/business/mapper/MenteeMapper.java
@@ -2,18 +2,19 @@ package com.kanyelings.telmah.mentormatchsb.business.mapper;
 
 import com.kanyelings.telmah.mentormatchsb.api.dto.MenteeDto;
 import com.kanyelings.telmah.mentormatchsb.data.entity.MenteeEntity;
+import lombok.SneakyThrows;
 import org.mapstruct.InheritInverseConfiguration;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @Mapper(componentModel = "spring", implementationPackage = "<PACKAGE_NAME>.impl")
 public interface MenteeMapper {
-    @Mapping(target = "image", ignore = true)
     MenteeDto mapMenteeEntityToDto(MenteeEntity menteeEntity);
 
     @InheritInverseConfiguration
     @Mapping(target = "menteeId", ignore = true)
-    @Mapping(target = "imagePath", ignore = true)
     MenteeEntity mapDtoToMentorEntity(MenteeDto menteeDto);
-
 }
